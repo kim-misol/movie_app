b5bd033c0eb6290891ff7f8aaa22cf85dd590de5
kim-misol <wonderfulpine@gmail.com>
2020-05-15 17:15:49 +0900

    Updates

---
+++ precache-manifest.eb2355397c8a6ccdedb28452a3c33ba2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af320ef8fc132cdc236a2703d3715df7",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "08e4375ce34a4be35bf7",
+    "url": "/movie_app/static/css/main.16c46b56.chunk.css"
+  },
+  {
+    "revision": "67c94d081af682a928a7",
+    "url": "/movie_app/static/js/2.31c51a38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app/static/js/2.31c51a38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08e4375ce34a4be35bf7",
+    "url": "/movie_app/static/js/main.0c6d59ea.chunk.js"
+  },
+  {
+    "revision": "77df4e0b724824eca1cd",
+    "url": "/movie_app/static/js/runtime-main.0b3e8895.js"
+  }
+]);
